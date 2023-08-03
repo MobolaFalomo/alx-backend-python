466fb020942046b0b5db145a25ea36c12b8ba5c2
Mobola Falomo <mobolafalomo@gmail.com>
2023-08-03 21:11:39 +0100

    Concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 ''' Description: takes a string str1 and a string str2 as arguments
     and returns a concatenated string
-        parameters: str1: string
-               str2: string
+        parameters: str1: str
+               str2: str
 '''
 
 
-def concat(str1: string, str2: string) -> string:
+def concat(str1: str, str2: str) -> str:
     """ Return a concatenated string. """
-    result: string = str1 + str2
+    result: str = str1 + str2
     return result
